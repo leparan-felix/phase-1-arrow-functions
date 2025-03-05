2ad0733c8a25cb6056242e009b65b1f0cc2813fa
leparan <leparanfelix16@gmail.com>
2025-03-05 12:42:36 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,16 @@
+// Function expression
+const greet = function(name) {
+    return `Hello, ${name}!`;
+  };
+  
+  // Arrow function
+  const add = (a, b) => a + b;
+  
+  // Function expression for divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Arrow function for square
+  const square = x => x * x;
+  
